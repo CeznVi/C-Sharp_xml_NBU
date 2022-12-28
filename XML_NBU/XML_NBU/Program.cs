8b1af6c1b0113cad09dcb9699e6e3e6b37514da7
Viktor <Cezn.Vi@gmail.com>
2022-12-28 21:32:29 +0200

    WIP

---
+++ XML_NBU/XML_NBU/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace XMLNBU
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тест загрузки ХМЛ");
+
+        }
+    }
+}
