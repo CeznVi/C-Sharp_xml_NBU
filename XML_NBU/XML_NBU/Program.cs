ac447231b0902c30018287611a62e47570184a9a
Viktor <Cezn.Vi@gmail.com>
2022-12-28 21:43:48 +0200

    WIP

--- XML_NBU/XML_NBU/Program.cs
+++ XML_NBU/XML_NBU/Program.cs
@@ -1,11 +1,55 @@
-﻿namespace XMLNBU
+﻿using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace XMLNBU
 {
     internal class Program
     {
-
-        static void Main(string[] args)
+        static void PrintNode(XmlNode node)
         {
-            Console.WriteLine("Тест загрузки ХМЛ");
+            Console.WriteLine($"Type={node.NodeType}".PadRight(20) +
+                $"Name={node.Name}".PadRight(20) +
+                $"Value={node.Value}".PadRight(20));
+
+            if (node.Attributes != null)
+            {
+                foreach (XmlNode item in node.Attributes)
+                {
+                    Console.WriteLine($"Type={item.NodeType}".PadRight(20) +
+                $"Name={item.Name}".PadRight(20) +
+                $"Value={item.Value}".PadRight(20));
+                }
+            }
+
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode item in node.ChildNodes)
+                {
+                    PrintNode(item);
+                }
+            }
+        }
+
+
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //XmlDocument xml = new();
+            //xml.Load("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+            //PrintNode(xml);
+
+            //Console.WriteLine("Тест загрузки ХМЛ");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string data = string.Empty;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                data = reader.ReadToEnd();
+
+            Console.WriteLine(data);
 
         }
     }
