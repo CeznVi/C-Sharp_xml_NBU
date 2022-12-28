82bcf94195e9770b271fe5f55b52340cca5ef69d
Viktor <Cezn.Vi@gmail.com>
2022-12-28 23:43:31 +0200

    WIP

---
+++ XML_NBU/Currency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_NBU
+{
+    /// <summary>
+    /// Клас контейнер для зберігання інформації про валюту
+    /// </summary>
+    public class Currency
+    {
+        public string? ShortName { get; set; }
+        public string? Name { get; set; }
+        public double Rate { get; set; }
+
+        /// <summary>
+        /// Конструктор без параметрів 
+        /// </summary>
+        public Currency() { }
+        /// <summary>
+        /// Конструктор з параметрами
+        /// </summary>
+        /// <param name="shortName">Коротка назва</param>
+        /// <param name="name">Повна назва</param>
+        /// <param name="rate">Курс</param>
+        public Currency(string? shortName, string? name, double rate)
+        {
+            ShortName = shortName;
+            Name = name;
+            Rate = rate;
+        }
+
+        /// <summary>
+        /// Повертає інформацію про валюту в текстовому вигляді 
+        /// </summary>
+        /// <returns>string (інфомація в текстовому вигляді про валюту)</returns>
+        public override string ToString()
+        {
+            return $"Абревіатура {ShortName}\n" +
+                   $"Повна назва {Name}\n" +
+                   $"Курс: {Rate}\n";
+        }
+
+
+    }
+}
--- XML_NBU/Program.cs
+++ XML_NBU/Program.cs
@@ -1,11 +1,11 @@
-﻿using System.Net;
-using System.Text;
+﻿using System.Text;
 using System.Xml;
 
-namespace XMLNBU
+namespace XML_NBU
 {
     internal class Program
     {
+
         static void PrintNode(XmlNode node)
         {
             Console.WriteLine($"Type={node.NodeType}".PadRight(20) +
@@ -31,20 +31,65 @@ namespace XMLNBU
             }
         }
 
+        static string GetValueTag(XmlTextReader xml, string Tag)
+        {
+            while (xml.Read())
+            {
+                if (xml.NodeType == XmlNodeType.Element &&
+                       xml.Name == Tag)
+                {
+                    xml.Read();
+                    return xml.Value;
+                }
+            }
+            return null;
+        }
 
         static void Main()
         {
             Console.OutputEncoding = Encoding.Unicode;
 
-            XmlDocument xml = new();
-            xml.Load("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
-            PrintNode(xml);
+            //лінк за яким працюємо
+            string link = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
+            //поле дати
+            string date = "";
+            //список данних про валюти
+            List<Currency> ExchRate = new();
 
-            Console.WriteLine("Тест загрузки ХМЛ");
+
+            using (XmlTextReader xmlR = new(link))
+            {
+                xmlR.WhitespaceHandling = WhitespaceHandling.None;
+                while (xmlR.Read())
+                {
+                    if (xmlR.NodeType == XmlNodeType.Element &&
+                       xmlR.Name == "currency")
+                    {
+                        Currency currency = new();
+                        currency.Name = new String(GetValueTag(xmlR, "txt"));
+                        currency.Rate = Convert.ToDouble(GetValueTag(xmlR, "rate").Replace('.', ','));
+                        currency.ShortName = new String(GetValueTag(xmlR, "cc"));
+                        ExchRate.Add(currency);
+                        
+                        //встановлення дати
+                        if (date.Length == 0)
+                            date = new String(GetValueTag(xmlR, "exchangedate"));
+
+                    }
+                }
+            }
+
+            foreach (Currency item in ExchRate)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"\n\n\nДАТА {date}");
 
 
-
-
+            //TODO
+            //1) SORT mekhanizm
+            //2) красивый принт
+            //3) делегаты на печать с условием
 
         }
     }
